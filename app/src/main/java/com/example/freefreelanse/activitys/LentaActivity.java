2928000c25b2627ad92d7b3d3bf5aeaeee48f7cd
Nick Borodayliuk <paragamelogict.gmail.com>
2019-05-13 13:55:06 +0300

    Add conection locall server

--- app/src/main/java/com/example/freefreelanse/activitys/LentaActivity.java
+++ app/src/main/java/com/example/freefreelanse/activitys/LentaActivity.java
@@ -2,6 +2,7 @@ package com.example.freefreelanse.activitys;
 
 import android.content.Intent;
 import android.nfc.Tag;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,10 +12,19 @@ import android.widget.Button;
 import com.example.freefreelanse.R;
 import com.example.freefreelanse.app.AppController;
 
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+
 public class LentaActivity extends AppCompatActivity implements View.OnClickListener {
+    public String string;
+    MyTask mt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +36,35 @@ public class LentaActivity extends AppCompatActivity implements View.OnClickList
     }
     @Override
     public void onClick(View v) {
+        mt = new MyTask();
+        mt.execute();
         switch (v.getId()){
             case R.id.button_go:
 
 
 
-              Log.i("s", "sssssss");
+              Log.i("s", " "+string);
                 break;
 
         }
 
+    }
+    class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+                DefaultHttpClient hc = new DefaultHttpClient();
+                ResponseHandler response = new BasicResponseHandler();
+                HttpGet http = new HttpGet("http://10.0.2.2:8080/demo/email?name=nick");
+                //получаем ответ от сервера
+            try {
+                string = (String) hc.execute(http, response);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
